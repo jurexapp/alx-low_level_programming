3ef478278d90bffb7db62130dbe0cf6386253130
jurexapp <mgbechetajustice@gmail.com>
2022-09-19 14:53:53 -0700

    c is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - my first C entry point
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char a[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(a);
+
+	return (0);
+}
