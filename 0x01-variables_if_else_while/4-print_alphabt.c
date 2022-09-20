6969003c4f40d05de035591a20ee2014dfbdbaf5
jurexapp <mgbechetajustice@gmail.com>
2022-09-20 10:11:36 +0100

    added  C source

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+		for (c = 97; c <= 122; c++)
+		{
+			if ((c == 101) || (c == 113))
+
+			{	continue;
+			}
+			putchar(c);
+
+		}
+
+			putchar(10);
+
+
+
+
+
+
+
+
+
+
+return (0);
+}
