6def283fafa02df81f00a221e6147678bd0d7ea2
jurexapp <mgbechetajustice@gmail.com>
2022-09-20 10:16:02 +0100

    added  C source

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+
+
+	{
+		putchar(c + '0');
+			c++;
+	}
+
+		putchar(10);
+
+
+
+
+
+
+
+
+
+
+return (0);
+}
