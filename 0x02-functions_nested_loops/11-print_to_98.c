f2b63d9cb6f8560e4e68c5ec60ab2db77ab5dc9a
jurexapp <mgbechetajustice@gmail.com>
2022-09-22 05:24:29 +0100

    added C sources

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,21 +21,33 @@ void print_to_98(int n)
 	// {
 	// 	printf("%d, ", n);
 	// }
-
-	while (n > 97 && n < !99)
+	if (n < 98)
+	{
+		for (n; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n > 98)
+	{
+		for (n; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else
 	{
 		printf("%d, ", n);
-		n--;
 	}
 
-	while (n < 98 && n > !97)
-	{
-		printf("%d, ", n);
-		n++;
-	}
+	// 	while (n < 98 && n > !97)
+	// 	{
+	// 		printf("%d, ", n);
+	// 		n++;
+	// 	}
 
-	if (n == 98)
-	{
-		printf("%d\n", n);
-	}
-}
+	// 	if (n == 98)
+	// 	{
+	// 		printf("%d\n", n);
+	// 	}
+	// }
