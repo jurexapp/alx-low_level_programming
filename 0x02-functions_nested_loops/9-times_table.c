7f37e8533c3a9592c2fa75f4728d9e9197a6172a
jurexapp <mgbechetajustice@gmail.com>
2022-09-22 08:33:45 +0100

    added C sources

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - prints times table
+ * Return:void
+ */
+
+void times_table(void)
+{
+
+    int a = 0;
+
+    int mul;
+
+    while (a <= 9)
+    {
+        int b = 0;
+
+        while (b <= 9)
+        {
+            mul = a * b;
+            if (b == 0)
+            {
+                _putchar('0' + mul);
+            }
+            else if (mul < 10)
+            {
+                _putchar(' ');
+                _putchar('0' + mul);
+            }
+            else
+            {
+                _putchar('0' + mul / 10);
+                _putchar('0' + mul % 10);
+            }
+
+            if (b < 9)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+            b++;
+        }
+        _putchar('\n');
+        a++;
+    }
+}
